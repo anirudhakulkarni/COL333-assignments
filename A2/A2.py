ec4f0b4665d32942002edeb2f22e1d18c6ef1dbc
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2021-10-25 00:03:43 +0530

    constraints graph prepared

--- A2/A2.py
+++ A2/A2.py
@@ -5,31 +5,23 @@ import sys
 import json
 
 
-class Tree():
+class Node():
     '''
-    Internal node in the bigger tree. Contains the mapping of nurses to shifts in each day and corresponds to a state.
-    Methods:
-        1. get_children: returns another state which is one step away from the current state.
-        
+    Node is a class that represents a day shift combination. Each node has a day, a shift, and a nurse.
     '''
 
-    def __init__(self, data, parent=None):
-        self.data = data
-        self.parent = parent
-        self.children = []
-        self.level = 0
-        self.path = []
-        self.path_cost = 0
-        self.heuristic = 0
-        self.visited = False
-
-    def add_child(self, obj):
-        self.children.append(obj)
-
-    # for debugging
+    def __init__(self, day, shift, total_nurses):
+        self.day = day
+        self.shift = shift
+        self.nurse = None
+        self.domains_available = [i for i in range(total_nurses)]
+        self.isFinal = False
 
     def __str__(self):
-        return str(self.data)
+        return str(self.day) + " " + str(self.shift) + " " + str(self.nurse)
+
+    def assignNurse(self, nurse):
+        self.nurse = nurse
 
 
 class Problem():
@@ -40,30 +32,38 @@ class Problem():
         2. solve: solves the problem using AC-3 algorithm
     '''
 
-
-    def __init__(self, nurses, days, mornings, afternoons, evenings):
-        self.nurses = nurses
-        self.days = days
-        self.mornings = mornings
-        self.afternoons = afternoons
-        self.evenings = evenings
+    def __init__(self, total_nurses, total_days, total_mornings, total_afternoons, total_evenings):
+        self.total_nurses = total_nurses
+        self.total_days = total_days
+        self.total_mornings = total_mornings
+        self.total_afternoons = total_afternoons
+        self.total_evenings = total_evenings
+        self.nodes = []
+        self.edges = {}
+        for i in range(self.total_days):
+            n1 = Node(i, 0, self.total_nurses)
+            n2 = Node(i, 1, self.total_nurses)
+            n3 = Node(i, 2, self.total_nurses)
+            self.nodes.append(n1)
+            self.nodes.append(n2)
+            self.nodes.append(n3)
+            # constraint 1: each nurse works at most one shift per day
+            self.edges[n1] = [n2, n3]
+            self.edges[n2] = [n1, n3]
+            self.edges[n3] = [n1, n2]
+        for i in range(self.total_days-1):
+            # constraint 2: no nurse works two morning shifts in a row
+            self.edges[self.nodes[i*3]].append(self.nodes[i*3+3])
+            # constraint 3: nurse can not work in morning shift if she has worked evening shift previous day
+            self.edges[self.nodes[i*3+2]].append(self.nodes[i*3+3])
 
     def check_validity(self):
         # check if the problem is valid
-        if self.nurses < 1 or self.days < 1 or self.mornings < 1 or self.afternoons < 1 or self.evenings < 1:
+        if self.total_nurses < 1 or self.total_days < 1 or self.total_mornings < 1 or self.total_afternoons < 1 or self.total_evenings < 1:
             return False
-        if self.mornings+self.afternoons+self.evenings > self.days:
+        if self.total_mornings+self.total_afternoons+self.total_evenings > self.total_days:
             return False
         return True
-
-    def solve(self):
-        # solve the problem using AC-3 algorithm
-        if not self.check_validity():
-            print("Invalid problem")
-            return [{}]
-        root = Tree(self.nurses*self.days)
-        self.backtrack(root)
-        return self.get_solution(root)
 
     def backtrack(self, root):
         # backtrack the tree
@@ -81,6 +81,7 @@ class Problem():
                         self.backtrack(child)
                     root.data[i][j] = 0
         return False
+
     def AC3(self, root):
         # AC-3 algorithm
         queue = []
@@ -97,11 +98,15 @@ class Problem():
                         if root.data[k][l] == 0:
                             queue.append((k, l))
         return root
-        
+
+
 if __name__ == "__main__":
     # take input from csv file from argument 1
     # nurses, days, mornings, afternoons, evenings
-    filename = sys.argv[1]
+    try:
+        filename = sys.argv[1]
+    except:
+        filename = "input_a.csv"
     with open(filename, 'r') as csvfile:
         reader = csv.reader(csvfile)
         row_list = []
---
+++ A2/notes.md
@@ -0,0 +1,10 @@
+# Automated Nurse Rostering System
+
+Constraints:
+
+1. m+a+e<=N
+2. mi<=m
+3. ai<=a
+4. ei<e
+
+A node class will contain each state
