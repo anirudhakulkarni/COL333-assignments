c6d7587776e18c4f5e57e2ae76dc7b41be32fc26
Deepanshu Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2021-11-24 12:07:26 +0530

    updates

--- A3/code.py
+++ A3/code.py
@@ -1,21 +1,20 @@
 import random
-def prob(): # function to return true with probability = 0.85
-	x = random.randint(1,100)
-	return x<=85
 
 
 
-actions = [(0,1), (0,-1), (0,1), (0,-1)] # north, south, east and west
+
+actions = [(0,1), (0,-1), (1,0), (-1,0)] # north, south, east and west
 
 
 
-def valueIteration():
+def valueIteration(): # TODO
 
 
-def policyIteration():
+def policyIteration(): # TODO
 
 def isSafe(x,y):
-	if(x>=0 and x<=5 and y>=0 and y<=5):
+	if(x>=0 and x<=5 and y>=0 and y<=5): # TODO
+		# wall conditions
 		return True
 	return False
 
@@ -35,14 +34,15 @@ def addToTransitionArray(state,desiredAction):
 
 
 transitionArray = {}
-hashMapForStates = {}
-x = 0
+# hashMapForStates = {}
+# x = 0
 for i1 in range(5):
 	for j1 in range(5):
 		for i2 in range(5):
 			for j2 in range(5):
-				hashMapForStates[state] = x
-				x = x + 1
+				state = (i1,j1,i2,j2)
+				# hashMapForStates[state] = x
+				# x = x + 1
 				addToTransitionArray(state,(0,1))
 				addToTransitionArray(state,(0,-1))
 				addToTransitionArray(state,(1,0))
@@ -50,12 +50,16 @@ for i1 in range(5):
 
 
 
-values = [0 for i in range 625]
+values = [0 for i in range 625] # value iteration array
+# values[hashMapOfASte()]
 
 
-
-
-
+# (i1,j1,i2,j2) => taxi at i1,j1 and passenget at i2,j2
+# (0,1) => north
+# actions = [(0,1), (0,-1), (1,0), (-1,0)] # north, south, east and west
+# T(s,a,s') = 
+# transitionArray((1,1,1,1), (0,1), (1,2,1,2)   => 0.85
+# transitionArray((1,1,1,1), (0,1), (1,2,1,2) 
 
 
 
