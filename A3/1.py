c54cb943544f3f8ffc04a3ec51542d306591605f
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2021-11-26 23:18:44 +0530

    val iter

--- A3/1.py
+++ A3/1.py
@@ -3,14 +3,13 @@ import random
 import numpy as np
 from collections import defaultdict
 
-from numpy.lib.arraysetops import isin
-
+random.seed(0)
 '''
 	Solving taxi problem with MDP
 '''
 
 # states:
-# (i1,j1,i2,j2) => taxi at i1,j1 and passenger at i2,j2
+# (i1,j1,i2,j2,isInTaxi) => taxi at i1,j1 and passenger at i2,j2, isin taxi?
 
 # actions:
 # actions = [(0,1), (0,-1), (1,0), (-1,0)] # north, south, east and west
@@ -20,50 +19,79 @@ from numpy.lib.arraysetops import isin
 # transitionArray((1,1,1,1), (0,1), (1,2,1,2)
 
 # reward function:
-# R(s,a,s') = R(s')
+# R(s,a,s')
 
 depots = [(0, 0), (0, 4), (3, 0), (4, 4)]
-actions = [(0, 1), (0, -1), (-1, 0), (1, 0)]  # north, south, east and west
-completeActions = [(0, 1), (0, -1), (-1, 0), (1, 0), "pickup", "putdown"]
-rewards = {}  # reward of states: 4 tuple (i1,j1,i2,j2)
+# actions = [(0, 1), (0, -1), (-1, 0), (1, 0)]  # north, south, east and west
+actions = [(0, 1), (0, -1), (-1, 0), (1, 0), "pickup", "putdown"]
 # transition probability of state, action, next state
 TransitionArray = defaultdict(int)
 values = defaultdict(int)  # value of states
+old_values = defaultdict(int)  # value of states
+
 policy = defaultdict(int)  # policy of states
 states = []
-isInTaxi = False
 
 
-def valueIteration(problem, epsilon):  # TODO
+class state:
+    def __init__(self, i1, j1, i2, j2, i3, j3, isInTaxi):
+        self.taxi = (i1, j1)
+        self.passenger = (i2, j2)
+        self.destination = (i3, j3)
+        self.isInTaxi = isInTaxi
+    # printing
+
+    def __str__(self):
+        return str(self.taxi) + " " + str(self.passenger) + " " + str(self.destination) + " " + str(self.isInTaxi)
+
+    def __eq__(self, other):
+        return self.taxi == other.taxi and self.passenger == other.passenger and self.destination == other.destination and self.isInTaxi == other.isInTaxi
+
+    def __hash__(self):
+        return hash(str(self))
+
+
+def reward_fuction(state1, action, state2):
+    if action == "putdwon" and state.passenger == state.destination:
+        return 20
+    if (action == "pickup" or action == "putdown") and state.taxi != state.passenger:
+        return -10
+    return -1
+
+
+def valueIteration(epsilon):  # TODO
     # implement value iteration
+    global values, old_values, TransitionArray, states, policy
     # problem = (passengerStart, passengerDestination, taxiStart)
     discount = 0.9
+    print("length of states", len(states))
+    iter = 0
+    max_iter = 1000
+    old_values = values.copy()
     # convergence criteria: max-norm distance between two consecutive value functions
-    while True:
+    while True and iter < max_iter:
         delta = 0
         for state in states:
-            oldValue = values[state]
+            print("starting", state)
             newValue = 0
             for action in actions:
-                if isSafe(state[0], state[1], state[0] + action[0], state[1] + action[1]):
-                    if isInTaxi:
-                        nextState = (state[0] + action[0], state[1] + action[1],
-                                     state[2] + action[0], state[3] + action[1])
-                    else:
-                        nextState = (
-                            state[0] + action[0], state[1] + action[1], state[0], state[1])
-                    currValue = 0
-                    for state2 in states:
+                currValue = 0
+                for state2 in states:
+                    if TransitionArray[state, action, state2] != 0:
                         currValue += TransitionArray[state, action, state2] * (
-                            rewards[state] + discount * values[state2])
-                    if currValue > newValue:
-                        newValue = currValue
-                        policy[state] = action
+                            reward_fuction(state, action, state2) + discount * old_values[state2])
+
+                if currValue > newValue:
+                    newValue = currValue
+                    policy[state] = action
             values[state] = newValue
-            delta = max(delta, abs(oldValue - newValue))
+            delta = max(delta, abs(values[state]-old_values[state]))
+        old_values = values.copy()
+        iter += 1
+        print(iter)
         if delta < epsilon:
             break
-    print(values)
+    # print(values)
 
 
 def policyIteration():  # TODO
@@ -89,71 +117,89 @@ def isSafe(oldx, oldy, newx, newy):
     return True
 
 
-def addToTransitionArray(state, desiredAction):
+def addToTransitionArray(state0, desiredAction):
+    if desiredAction == "pickup":
+        TransitionArray[state0, desiredAction, state0] = 1
+        return
+    if desiredAction == "putdown":
+        TransitionArray[state0, desiredAction, state0] = 1
+        return
+
     for action in actions:
-        newX = state[0] + action[0]
-        newY = state[1] + action[1]
-        if(isSafe(state[0], state[1], newX, newY)):
-            state1 = (newX, newY, state[2], state[3])
-            if(action == desiredAction):
-                TransitionArray[state, action, state1] = 0.85
+        if action != "pickup" and action != "putdown":
+
+            newX, newY = state0.taxi[0]+action[0], state0.taxi[1]+action[1]
+            if(isSafe(state0.taxi[0], state0.taxi[1], newX, newY)):
+                state1 = state(newX, newY, state0.passenger[0], state0.passenger[1],
+                               state0.destination[0], state0.destination[1], state0.isInTaxi)
+                if(action == desiredAction):
+                    TransitionArray[state0, action, state1] = 0.85
+                else:
+                    TransitionArray[state0, action, state1] = 0.05
             else:
-                TransitionArray[state, action, state1] = 0.05
-        else:
-            TransitionArray[state, action, state] = 1
+                TransitionArray[state0, action, state0] = 1
 
 
 # transition function
-def initTransitionFunction():
-    global states
+def initTransitionFunction(passengerDestination):
+    global states, values, TransitionArray, policy
+    global policy
     x = 0
     for i1 in range(5):
         for j1 in range(5):
             for i2 in range(5):
                 for j2 in range(5):
-                    state = (i1, j1, i2, j2)
-                    states += [state]
-                    rewards[state] = 0
-                    policy[state] = random.choice(actions)
-                    values[state] = 0
-                    for action in actions:
-                        addToTransitionArray(state, action)
+                    for b in {True, False}:
+                        mystate = state(
+                            i1, j1, i2, j2, passengerDestination[0], passengerDestination[1], b)
+                        states += [mystate]
+                        policy[mystate] = random.choice(actions)
+                        if (i1, j1, i2, j2, passengerDestination[0], passengerDestination[1], b) == (0, 2, 4, 4, 0, 4, False):
+                            print("found!")
+                            print(policy[mystate])
+                            print(mystate)
+                        values[mystate] = 0
+                        for action in actions:
+                            addToTransitionArray(mystate, action)
+    print(policy[state(0, 2, 4, 4, 0, 4, False)])
 
 
-def initReward(passengerDestination):
-    rewards[passengerDestination] = 1
-
-
-def takeAction(passengerLoc, passengerDestination, taxiLoc):
+def takeAction(currState):
     # given policy and start end position, simulate the taxi
     # return the end position
     isInTaxi = False
     global actions
     while True:
-        render(passengerLoc, passengerDestination, taxiLoc)
-        print(passengerLoc, passengerDestination, taxiLoc)
-        currState = (taxiLoc[0], taxiLoc[1],
-                     passengerLoc[0], passengerLoc[1])
-        rand_choice = random.random()
-        if(rand_choice < 0.85):
-            action = policy[currState]
+        render(currState.taxi, currState.passenger, currState.destination)
+        print(currState.taxi, currState.passenger, currState.destination)
+        action = policy[currState]
+        print(action)
+        if action == "pickup":
+            if currState.taxi == currState.passenger:
+                currState.isInTaxi = True
+                continue
+        elif action == "putdown":
+            if currState.taxi == currState.passenger:
+                currState.isInTaxi = False
+                if currState.passenger == currState.destination:
+                    return currState
+                continue
         else:
-            temp = policy[currState]
-            actions.remove(temp)
-            action = random.choice(actions)
-            actions.append(temp)
-        if(taxiLoc == passengerDestination and isInTaxi):
-            return "Reached"
-        if type(action) == str:
-            if action == "pickup":
-                isInTaxi = True
-            if action == "putdown":
-                isInTaxi = False
-        else:
-            if(isSafe(taxiLoc[0], taxiLoc[1], taxiLoc[0]+action[0], taxiLoc[1]+action[1])):
-                taxiLoc += action
-                if isInTaxi:
-                    passengerLoc += action
+            rand_choice = random.random()
+            if(rand_choice < 0.85):
+                action = action
+            else:
+                temp = policy[currState]
+
+                actions.remove(temp)
+                action = random.choice(actions)
+                actions.append(temp)
+            newX, newY = currState.taxi[0] + \
+                action[0], currState.taxi[1]+action[1]
+            if(isSafe(currState.taxi[0], currState.taxi[1], newX, newY)):
+                currState.taxi = (newX, newY)
+                if currState.isInTaxi:
+                    currState.passenger = currState.taxi
             else:
                 continue
 
@@ -194,41 +240,28 @@ def render(passengerLoc, passengerDestination, taxiLoc):
     pygame.time.wait(5)
 
 
-# def render(passengerLoc, passengerDestination, taxiLoc):
-#     for i in range(5):
-#         for j in range(5):
-#             if (i, j) == passengerLoc:
-#                 print("P", end=" ")
-#             elif (i, j) == passengerDestination:
-#                 print("D", end=" ")
-#             elif (i, j) == taxiLoc:
-#                 print("T", end=" ")
-#             else:
-#                 print("-", end=" ")
-#         print()
-
-
-def simulate():
+def eposide():
 
     # randomly generate starting depot for passenger, select different destination depot for passenger and starting location for taxi
     passengerStart = depots[random.randint(0, 3)]
     depots.remove(passengerStart)
     passengerDestination = depots[random.randint(0, 2)]
     taxiStart = (random.randint(0, 4), random.randint(0, 4))
-    print(passengerStart, passengerDestination, taxiStart)
+    print(taxiStart, passengerStart, passengerDestination)
     depots.append(passengerStart)
-    isPassengerInTaxi = False
 
     # initialize reward function
-    initReward(passengerDestination)
-    initTransitionFunction()
+    print("initializing reward function")
+    initTransitionFunction(passengerDestination)
+    print("Starting value iteration")
+    valueIteration(0.001)
 
-    problem = (passengerStart, passengerDestination, taxiStart)
-    valueIteration(problem, 0.001)
-    print(problem)
-    takeAction(passengerStart, passengerDestination, taxiStart)
+    iniState = state(taxiStart[0], taxiStart[1], passengerStart[0],
+                     passengerStart[1], passengerDestination[0], passengerDestination[1], False)
+    print("Starting simulation as per policy")
+    takeAction(iniState)
 
 
 if __name__ == '__main__':
-    simulate()
+    eposide()
     # print(TransitionArray)
